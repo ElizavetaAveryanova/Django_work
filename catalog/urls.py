74bf8e4895d61cc0d88d3db2e50af7370d64c16e
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2024-04-22 23:35:24 +0500

    home_work_21.1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, category_products, product_description
+from catalog.views import ContactsPageView, CategoryListView, ProductListView, ProductDetailsView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('<int:pk>/products/', category_products, name='category_products'),
-    path('<int:pk>/product/', product_description, name='product_description')
+    path('', CategoryListView.as_view(), name='home'),
+    path('contacts/', ContactsPageView.as_view(), name='contacts'),
+    path('<int:pk>/products/', ProductListView.as_view(), name='category_products'),
+    path('<int:pk>/product/', ProductDetailsView.as_view(), name='product_description')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,42 +1,54 @@
 from django.shortcuts import render
+from django.views.generic import ListView, TemplateView, DetailView
 
 from catalog.models import Product, Category
 
 
-def home(request):
-    category_list = Category.objects.all()
-    context = {
-        'object_list': category_list,
+class CategoryListView(ListView):
+    model = Category
+    extra_context = {
         'title': 'Доступные категории товаров'
     }
-    return render(request, 'catalog/home.html', context)
+
+class ContactsPageView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method == "POST":
+            name = request.POST.get('name')
+            phone = request.POST.get('phone')
+            message = request.POST.get('message')
+
+            print(f'Имя - {name}\n'
+                  f'Телефон - {phone}\n'
+                  f'Сообщение: {message}')
+
+        return render(request, 'catalog/contacts.html')
 
 
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
+class ProductListView(ListView):
+    model = Product
 
-        print(f'Имя - {name}\n'
-              f'Телефон - {phone}\n'
-              f'Сообщение: {message}')
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(category_id=self.kwargs.get('pk'))
+        return queryset
 
-    return render(request, 'catalog/contacts.html')
+    def get_context_data(self, *args, **kwargs):
+        category_item = Category.objects.get(pk=self.kwargs.get('pk'))
+        context_data = super().get_context_data(*args, **kwargs)
+        context_data['title'] = f'Категория: {category_item.name}'
+        return context_data
 
 
-def category_products(request, pk):
-    category_item = Category.objects.get(pk=pk)
-    context = {
-        'object_list': Product.objects.filter(category_id=pk),
-        'title': f'Категория: {category_item.name}'
+
+class ProductDetailsView(DetailView):
+    model = Product
+    template_name = 'catalog/product.html'
+    extra_context = {
+        'title': 'Описание товара'
     }
-    return render(request, 'catalog/products.html', context)
 
 
-def product_description(request, pk):
-    product = Product.objects.get(pk=pk)
-    context = {
-        'object': product,
-    }
-    return render(request, 'catalog/product.html', context)
+
+
